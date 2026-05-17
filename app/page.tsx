d7c3d075cc2d45ac57fb585eaf848ec521ca896e
Alina_Goldberg <serg.gold.berg.88@gmail.com>
2026-05-17 23:40:52 +0200

    add Home p + hero section(ended)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import css from "./Home.module.css";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Home page</h1>
+    <main className={css.hero}>
+      <div className={css.overlay} />
+      <div className={css.content}>
+        <h1 className={css.title}>Find your perfect rental car</h1>
+        <p className={css.subtitle}>
+          Reliable and budget-friendly rentals for any journey
+        </p>
+        <Link href="/catalog" className={css.button}>
+          View Catalog
+        </Link>
+      </div>
     </main>
   );
 }
